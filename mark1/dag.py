199c39638def047bcad18dddae4b218f529c468a
Sumit Vishwakarma <sviscoding@gmail.com>
2026-04-17 16:44:01 +0530

    dag

---
+++ mark1/dag.py
@@ -0,0 +1,38 @@
+from airflow.sdk import dag, task 
+
+@dag(
+        dag_id='Xcom_OldSchool'
+)
+def myDag():
+
+    @task.python
+    def extract():
+
+        f_name='sumit'
+        l_name='vishwakarma'
+
+        return {
+                    'first':f_name,
+                    'second':l_name
+        }
+    
+
+    @task.python
+    def transform(data):
+
+        fullName=data['first'].upper()+' '+data['second'].upper()
+
+        return fullName
+    
+    @task.python
+    def load(result):
+
+        print(f"Hello {result}")
+    
+    first=extract()
+    second=transform(first)
+    third=load(second)
+
+    first >> second >> third
+
+myDag()
